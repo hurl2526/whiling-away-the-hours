f9289d2126d49ee12a9e974b27ed92b24f371cef
Patrick <patrick.hurley@codeimmersives.com>
2020-02-12 17:26:25 -0500

    still working

--- main.js
+++ main.js
@@ -86,13 +86,54 @@ function bOff(){
   bOff();
 addChallengeBanner(8);
 
+// function number8 (num){
+//   let something = num;
+//   while (something > 0) {
+//     console.log(something);
+//     something = something - 3;
+//   }
+//   }
 function number8 (num){
-  let something = num;
-  while (something > 0) {
-    console.log(something);
-    something = something - 3;
+  while (num > 0) {
+    console.log(num);
+    num = num - 3;
   }
   }
 number8(20);
 addChallengeBanner(9);
+
+function helloPlus (){
+  let num = 1;
+  while (num <=7) {
+    console.log(num +'.'+ 'Hello!');
+    num=num + 1;
+  }
+  } 
+helloPlus();
 addChallengeBanner(10);
+
+function myString (words){
+  let num = 1;
+  while (num <=7) {
+    console.log(num + '.' + ' ' + words);
+    num=num + 1;
+  }
+  } 
+  myString('This is hard')
+
+addChallengeBanner(11);
+
+num2 = 1
+function weird(string, num){
+  while (num2 <= num){
+    console.log(string)
+    num2= num2 + 1
+  }
+}
+weird('hello', 4);
+
+addChallengeBanner(12);
+addChallengeBanner(13);
+addChallengeBanner(14);
+addChallengeBanner(15);
+addChallengeBanner(16);
